b1f11a38a01617e77ad508ad1120fe99a339678b
Миша Тарачков <mishklgpmi@mail.ru>
2015-07-12 01:27:15 +0300

    Update lab7_pwm.cpp

--- atXmega128a1/lab7_pwm/lab7_pwm.cpp
+++ atXmega128a1/lab7_pwm/lab7_pwm.cpp
@@ -1,23 +1,24 @@
-#define F_CPU 2000000UL
+#define  F_CPU    16000000UL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
 int main(void) {
-	PORTE.DIRSET = 0b00001111;
-	//включаем 4 канала ШИМ
-	TCE0.CTRLB = TC_WGMODE_DS_B_gc | TC0_CCAEN_bm | TC0_CCBEN_bm | TC0_CCCEN_bm | TC0_CCDEN_bm;
-	//устанавливаем период
-	TCE0.PER = 1000;
-	//частота не делится
-	TCE0.CTRLA = TC_CLKSEL_DIV1_gc;
-	while(1) {	
-		for(int i=0;i<300;i++){
+	PORTE.DIRSET = 0b00000001;
+	
+	TCE0.CTRLB = TC_WGMODE_SS_gc | TC0_CCAEN_bm;
+	TCE0.PER = 39999;
+	TCE0.CTRLA = TC_CLKSEL_DIV8_gc;
+	
+	int i=90;
+	while(1) {
+		i++;
+		if(i>620){		//границы 90-620
+			i=90;
 			TCE0.CCA = i;
-			TCE0.CCB = i+200;
-			TCE0.CCC = i+400;
-			TCE0.CCD = i+600;
-			_delay_ms(1);
-		}
+			_delay_ms(50);
+		}		
+		TCE0.CCA = i;
+		_delay_ms(2);
 	}
 }
