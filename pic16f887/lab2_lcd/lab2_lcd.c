3121bc70b0b5ee907a51905d6ea84bad4697bc52
Миша Тарачков <mishklgpmi@mail.ru>
2015-07-19 19:42:46 +0300

    Update lab2_lcd.c

--- pic16f887/lab2_lcd/lab2_lcd.c
+++ pic16f887/lab2_lcd/lab2_lcd.c
@@ -3,28 +3,20 @@
 
 #include    <htc.h>
 
-//определяем выводы
 #define    LCD_RS RA3
 #define    LCD_RW RA4
 #define    LCD_EN RA5
+#define  LCD_DATA PORTB
 
-//линия передачи данных
-#define LCD_DATA PORTB
-
-//для обновления LCD, команды выполняются последовательно
+//стробирование (макрос, операции выполняются последовательно)
 #define LCD_STROBE() ((LCD_EN = 1),(LCD_EN = 0))
 
 __CONFIG(WDTDIS & UNPROTECT & MCLREN & LVPDIS & HS);
 
-//функция задержки
-void delay(int value){
-	for (int i = 0; i<value*1000; i++){}
-}
-
-//функция записи в 4-х битном режиме
+//запись в 4-хбитном режиме
 void lcd_write(unsigned char c)
 {
-	delay(4);
+	__delay_ms(50);
 	LCD_DATA = ((c >> 4) & 0x0F);
 	LCD_STROBE();
 	LCD_DATA = (c & 0x0F);
@@ -36,62 +28,60 @@ void lcd_clear(void)
 {
 	LCD_RS = 0;
 	lcd_write(0x1);
-	delay(4);
+        __delay_ms(50);
 }
 
-//функция записи строки
+//запись строки
 void lcd_puts(const char * s)
 {
 	LCD_RS = 1;
-	while (*s)
-		lcd_write(*s++);
+	while(*s)
+            lcd_write(*s++);
 }
 
-//функция записи одного символа
+//запись символа
 void lcd_putch(char c)
 {
 	LCD_RS = 1;
 	lcd_write(c);
 }
 
-//перемещение к выбранной позиции
+//переход к указанному адресу
 void lcd_goto(unsigned char pos)
 {
 	LCD_RS = 0;
 	lcd_write(0x80 + pos);
 }
 
-//инициализация дисплея, 4-х битный режим
+//инициализация контроллера дисплея
 void lcd_init()
 {
-	//отключаем альтернативные функции
+	//отключаем альтернативные функции выводов порта А
 	ANSEL = 0x00;
-	//настроиваем порты A и B на выход
+	//выводы портов А и В настраиваем на выход
 	TRISA = 0;
 	TRISB = 0;
+
+        //инициализируем управляющие выводы
 	LCD_RS = 0;
 	LCD_EN = 0;
 	LCD_RW = 0;
 
-	delay(15);
-	LCD_DATA = 3;
-	LCD_STROBE();
-	delay(5);
-	LCD_STROBE();
-	delay(1);
-	LCD_STROBE();
-	delay(1);
-	LCD_DATA = 2;
-	LCD_STROBE();
+        //задержка для того, чтобы питание стало стабильным
+	__delay_ms(100);
 
-	lcd_write(0x28); // Set interface length
-	lcd_write(0xF);  // Display On, Cursor On, Cursor Blink
-	lcd_clear();     // Clear screen
-	lcd_write(0x6);  // Set entry Mode
+        //0b00101000 - 4-хбитная шина данных, 2 строки, 5х7 пикселей на 1 символ
+        lcd_write(0x28);
+        //0b00001111 - включаем дисплей, включаем курсор, курсор мигает
+	lcd_write(0xF);
+        //очищаем экран
+	lcd_clear();
+        //инкремент адреса, дисплей не сдвигается
+	lcd_write(0x6);
 }
 void main() {
 	lcd_init();
-	lcd_goto(0x00);	//переход на вторую линию
+	lcd_goto(0x00);
 	lcd_puts("Hello, Vova!");
 	for (;;);
 }
